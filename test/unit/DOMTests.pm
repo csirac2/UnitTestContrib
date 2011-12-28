1302761ce1672647377591a6d2930eaa4fd22adf
Paul.W Harvey <Paul.W.Harvey@csiro.au>
2011-12-28 22:24:20 +1100

    pseduo-macro tests

--- test/unit/DOMTests.pm
+++ test/unit/DOMTests.pm
@@ -11,7 +11,7 @@ use Benchmark qw(:hireswallclock);
 use Foswiki::DOM();
 use constant TRACE => 0;
 
-sub test_verbatim {
+sub test_verbatim_nested {
     my ($this) = @_;
 
     Foswiki::DOM->new(<<'HERE');
@@ -23,6 +23,86 @@ a<verbatim class="tml">b
 HERE
 
     return;
+}
+
+sub test_verbatim_stray_open_tag {
+    my ($this) = @_;
+    my $input = <<'HERE';
+a<verbatim><verbatim class="tml">b
+<verbatim class="html">x
+0\
+1\
+</verbatim>y</verbatim>z
+HERE
+    $this->annotate("Length of input: " . length($input) . "\n");
+    Foswiki::DOM->new($input);
+
+    return;
+}
+
+sub test_verbatim_stray_close_tag {
+    my ($this) = @_;
+
+    Foswiki::DOM->new(<<'HERE');
+a<verbatim class="tml">b
+<verbatim class="html">x
+0\
+1\
+</verbatim>y</verbatim>z</verbatim>
+HERE
+
+    return;
+}
+
+sub test_verbatim_stray_close_tag {
+    my ($this) = @_;
+
+    Foswiki::DOM->new(<<'HERE');
+<a \
+href="\
+something\
+"\
+>b</a>
+
+Trai\ 
+ling white\ 
+space
+
+Dou\\ble sla\\shes
+HERE
+
+    return;
+}
+
+sub test_VAR {
+    my ($this) = @_;
+
+    Foswiki::DOM->new('%VAR%');
+}
+
+sub test_VAR_params {
+    my ($this) = @_;
+
+    Foswiki::DOM->new(<<'HERE');
+%VAR{a="b" c="d
+    with
+    newlines\
+ escaped"}%
+HERE
+}
+
+sub test_nested_VAR {
+    my ($this) = @_;
+
+    Foswiki::DOM->new(<<'HERE');
+%VAR{a="b" c="d
+    with %NESTED% %MACROS%
+    newlines\
+ escaped"}%
+
+%%FOO%RCH%{
+}%
+HERE
 }
 
 1;
