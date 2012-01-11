f949ac013731f2a89926c381dd11a5a5283ba191
Paul.W Harvey <Paul.W.Harvey@csiro.au>
2012-01-12 00:58:27 +1100

    progress

--- test/unit/DOMTests.pm
+++ test/unit/DOMTests.pm
@@ -105,4 +105,76 @@ sub test_nested_VAR {
 HERE
 }
 
+sub _generate_timing_input {
+    my ($this, $times) = @_;
+
+    return '<a b="1">b</a>' x $times;
+}
+
+# input =~ s/foo/bar/g
+# 
+sub test_timing_replace_empty {
+    my ($this) = @_;
+    my $input = $this->_generate_timing_input(100000);
+    my %stash;
+    my $string = '';
+    my $benchmark = timeit(10000, sub {
+        $input =~ s/(<a b="(\d+?)">(.*?)<\/a>)/$stash{$2} = $3; $string;/gem;
+    });
+
+    print timestr($benchmark) . "\n";
+
+    return;
+}
+
+sub test_timing_replace_fixedwidth {
+    my ($this) = @_;
+    my $input = $this->_generate_timing_input(100000);
+    my %stash;
+    my $string = ' ' x length($this->_generate_timing_input(1));
+    my $benchmark = timeit(100, sub {
+        $input =~ s/(<a b="(\d+?)">(.*?)<\/a>)/$stash{$2} = $3; $string;/gem;
+    });
+
+    print timestr($benchmark) . "\n";
+
+    return;
+}
+
+sub test_timing_replace_substr_lvalue {
+    my ($this) = @_;
+    my $input = $this->_generate_timing_input(5000);
+    my %stash;
+    my $string = ' ' x length($this->_generate_timing_input(1));
+    my $stringlength = length($string);
+    my $benchmark = timeit(1, sub {
+        while ($input =~ /(<a b="(\d+?)">(.*?)<\/a>)/g) {
+            $stash{$2} = $3;
+            substr($input, pos($input) - $stringlength, $stringlength) = $string;
+        }
+    });
+
+    print timestr($benchmark) . "\n";
+
+    return;
+}
+
+sub test_timing_replace_substr {
+    my ($this) = @_;
+    my $input = $this->_generate_timing_input(5000);
+    my %stash;
+    my $string = ' ' x length($this->_generate_timing_input(1));
+    my $stringlength = length($string);
+    my $benchmark = timeit(1, sub {
+        while ($input =~ /(<a b="(\d+?)">(.*?)<\/a>)/g) {
+            $stash{$2} = $3;
+            substr($input, pos($input) - $stringlength, $stringlength, $string);
+        }
+    });
+
+    print timestr($benchmark) . "\n";
+
+    return;
+}
+
 1;
